766b5533ab7f4c51bd096934ddad1afea50fc59a
Artem <artmurashko@yandex.ru>
2021-04-10 15:01:31 +0300

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/WendexTaxi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WendexTaxi.dir/main.cpp.o"
+  "WendexTaxi"
+  "WendexTaxi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WendexTaxi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/WendexTaxi.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for WendexTaxi.
+# This may be replaced when dependencies are built.
