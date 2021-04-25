c4d20683c0d5cfb8c731637831414c225a4be5ec
Artem <artmurashko@yandex.ru>
2021-04-25 22:49:14 +0300

    Delete all files

--- WendexTaxi.h
+++ WendexTaxi.h
@@ -7,21 +7,9 @@
 #define WENDEXTAXI_WENDEXTAXI_H
 
 // Include all files from the Cars folder
-#include "Cars/Business.h"
-#include "Cars/Comfort.h"
-#include "Cars/ComfortPlus.h"
-#include "Cars/Economy.h"
-#include "Cars/Car.h"
-#include "Cars/CarType.h"
-#include "Cars/CarColor.h"
 
 // Include all files from the Users folder
-#include "Users/Driver.h"
-#include "Users/Passenger.h"
-#include "Users/User.h"
 
 // Include all files from the Gateways folder
-#include "Gateways/DriverGateway.h"
-#include "Gateways/PassengerGateway.h"
 
 #endif //WENDEXTAXI_WENDEXTAXI_H
--- main.cpp
+++ main.cpp
@@ -4,63 +4,16 @@
 using namespace std;
 
 /* ----- Cars ----- */
-Economy car1("Ranault Logan", Coordinates(), CarColor::red, "AB123C");
-Economy car2("Lada Granta", Coordinates(), CarColor::yellow, "AK133E");
-Comfort car3("Kia Rio", Coordinates(), CarColor::white, "EA873M");
-Comfort car4("Hyondai Solaris", Coordinates(), CarColor::black, "EA823M");
-ComfortPlus car5("Kia Cerato", Coordinates(), CarColor::black, "MJ122J");
-Business car6("Kia Optima", Coordinates(), CarColor::black, "EK222X");
 
 /* ----- Drivers ----- */
-vector <Driver> drivers = {
-        Driver("Artem", 4.5, "+123", &car1),
-        Driver("John", 2.7, "+231", &car3),
-        Driver("Alex", 3.8, "+443", &car5),
-        Driver("Ivan", 4.2, "+777", &car6),
-};
 
 /* ----- Passengers ----- */
-vector <Passenger> passengers = {
-        Passenger("Alena", 4.92, "+898"),
-        Passenger("Katya", 4.8, "+897"),
-        Passenger("Vsevolod", 4.98, "+896"),
-        Passenger("Sergey", 4.9, "+895"),
-};
 
 /* ----- Gateways ----- */
-DriverGateway driverGateway;
-PassengerGateway passengerGateway;
 
 using namespace std;
 
 int main() {
-    driverGateway.setOnlineStatus(drivers[0], true); // Set status of driver number 1 to online
-    driverGateway.setOnlineStatus(drivers[1], true); // Set status of driver number 2 to online
-    driverGateway.setOnlineStatus(drivers[2], true); // Set status of driver number 3 to online
-    driverGateway.setOnlineStatus(drivers[3], true); // Set status of driver number 4 to online
 
-    driverGateway.checkAvailableOrders();
-
-    /*
-     * Passenger Katya create order with type economy.
-     * Since Artem has car economy, he took it.
-    */
-    passengerGateway.createOrder(passengers[1], Address("Moscow, RedSquare", Coordinates()), Address("Moscow, MoscowCity", Coordinates()), CarType::economy);
-
-    driverGateway.setOnlineStatus(drivers[0], false); // Set status of Artem to offline
-
-    // Passenger Vsevolod create order with type economy.
-    // No one accept, since there are all drivers offline with economy cars.
-    passengerGateway.createOrder(passengers[2], Address("Moscow, RedSquare", Coordinates()), Address("Moscow, MoscowCity", Coordinates()), CarType::economy);
-
-
-    // Make Artem online
-    driverGateway.setOnlineStatus(drivers[0], true);
-
-    // Checking available orders (See Vsevolod's order)
-    driverGateway.checkAvailableOrders();
-
-    // Artem want to find available orders and accept Vsevolod's order
-    driverGateway.findOrder(drivers[0]);
     return 0;
 }
